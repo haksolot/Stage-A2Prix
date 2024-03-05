96d2bcfc67f01374e129c8503ab8bdd92f91053d
haksolot <sean.lamet.mail@gmail.com>
2024-03-05 22:34:54 +0100

    update on work for offer editing

--- front/script/offer-interactions.js
+++ front/script/offer-interactions.js
@@ -69,85 +69,6 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     // Ajoutez un gestionnaire d'événements de clic à chaque bouton "Modifier"
-//     var editButtons = document.querySelectorAll('.edit-button');
-    
-//     // Définissez une classe pour le mode édition
-//     var editModeClass = 'edit-mode';
-
-//     // Ajoutez un gestionnaire d'événements de clic à chaque bouton "Modifier"
-//     editButtons.forEach(function(button) {
-//         button.addEventListener('click', function() {
-//             // Vérifiez si le bouton est en mode édition
-//             var isInEditMode = button.classList.contains(editModeClass);
-
-//             // Récupérez les éléments à modifier
-//             var title = button.closest('.offer-container').querySelector('.offer-title');
-//             var h1Title = button.closest('.offer-container').querySelector('.offer-h1');
-//             var h2Title = button.closest('.offer-container').querySelector('.offer-h2');
-//             var address = button.closest('.offer-container').querySelector('.offer-address');
-//             var description = button.closest('.offer-container').querySelector('.offer-description');
-//             var caracElements = button.closest('.offer-container').querySelectorAll('.offer-carac p');
-
-//             if (!isInEditMode) {
-//                 // Cacher les éléments d'affichage
-//                 h1Title.style.display = 'none';
-//                 h2Title.style.display = 'none';
-//                 address.style.display = 'none';
-//                 description.style.display = 'none';
-//                 caracElements.forEach(function(caracElement) {
-//                     caracElement.style.display = 'none';
-//                 });
-
-//                 // Afficher les champs d'entrée correspondants
-//                 var titleH1Input = title.querySelector('.offer-h1-input');
-//                 var titleH2Input = title.querySelector('.offer-h2-input');
-//                 var addressInput = button.closest('.offer-container').querySelector('.offer-address-input');
-//                 var descriptionInput = button.closest('.offer-container').querySelector('.offer-description-input');
-//                 var caracInputs = button.closest('.offer-container').querySelectorAll('.offer-carac-input');
-                
-//                 titleH1Input.style.display = 'block';
-//                 titleH2Input.style.display = 'block';
-//                 addressInput.style.display = 'block';
-//                 descriptionInput.style.display = 'block';
-//                 caracInputs.forEach(function(caracInput) {
-//                     caracInput.style.display = 'block';
-//                 });
-
-//                 // Ajoutez la classe pour le mode édition
-//                 button.classList.add(editModeClass);
-//             } else {
-//                 // Afficher à nouveau les éléments d'affichage et masquer les champs d'entrée
-//                 h1Title.style.display = 'block';
-//                 h2Title.style.display = 'block';
-//                 address.style.display = 'block';
-//                 description.style.display = 'block';
-//                 caracElements.forEach(function(caracElement) {
-//                     caracElement.style.display = 'block';
-//                 });
-
-//                 var titleH1Input = title.querySelector('.offer-h1-input');
-//                 var titleH2Input = title.querySelector('.offer-h2-input');
-//                 var addressInput = button.closest('.offer-container').querySelector('.offer-address-input');
-//                 var descriptionInput = button.closest('.offer-container').querySelector('.offer-description-input');
-//                 var caracInputs = button.closest('.offer-container').querySelectorAll('.offer-carac-input');
-
-//                 titleH1Input.style.display = 'none';
-//                 titleH2Input.style.display = 'none';
-//                 addressInput.style.display = 'none';
-//                 descriptionInput.style.display = 'none';
-//                 caracInputs.forEach(function(caracInput) {
-//                     caracInput.style.display = 'none';
-//                 });
-
-//                 // Retirez la classe pour le mode édition
-//                 button.classList.remove(editModeClass);
-//             }
-//         });
-//     });
-// });
-
 document.addEventListener('DOMContentLoaded', function() {
     // Ajoutez un gestionnaire d'événements de clic à chaque bouton "Modifier"
     var editButtons = document.querySelectorAll('.edit-button');
@@ -179,6 +100,14 @@ document.addEventListener('DOMContentLoaded', function() {
                     caracElement.style.display = 'none';
                 });
 
+                document.querySelector('.offer-star-holder').style.display = 'none';
+                document.querySelector('.offer-like-container').style.display = 'none';
+                document.querySelector('.offer-sector').style.display = 'none';
+                document.querySelector('.offer-money').style.display = 'none';
+                document.querySelector('.offer-people').style.display = 'none';
+                document.querySelector('.offer-line-vertical').style.display = 'none';
+                document.querySelector('.offer-time').style.display = 'none';
+    
                 // Afficher les champs d'entrée correspondants
                 var titleH1Input = title.querySelector('.offer-h1-input');
                 var titleH2Input = title.querySelector('.offer-h2-input');
@@ -243,6 +172,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 caracInputs.forEach(function(caracInput) {
                     caracInput.style.display = 'none';
                 });
+
 
                 // Retirez la classe pour le mode édition
                 button.classList.remove(editModeClass);
