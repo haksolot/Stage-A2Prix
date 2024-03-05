766edda11bcf7ff21fa013285ee6544bb42f0b79
Baptiste Jousseaume <baptiste.jousseaume@viacesi.fr>
2024-03-05 13:58:17 +0100

    ajout de plusieurs remplissage de tables

---
+++ database/Filling Addresses.sql
@@ -0,0 +1,27 @@
+INSERT INTO Localisation (Adresse, ID_Ville)
+VALUES
+('1 Rue de la Liberté', 1), -- Aix-en-Provence
+('25 Rue des Fleurs', 2), -- Angoulême
+('10 Avenue des Roses', 3), -- Arras
+('5 Place du Commerce', 4), -- Bordeaux
+('15 Rue de la Mer', 5), -- Brest
+('30 Boulevard des Arts', 6), -- Caen
+('12 Rue de la République', 7), -- Dijon
+('8 Rue des Montagnes', 8), -- Grenoble
+('20 Rue des Coquelicots', 9), -- La Rochelle
+('7 Rue de la Gare', 10), -- Le Mans
+('3 Avenue de la Paix', 11), -- Lille
+('14 Rue du Bonheur', 12), -- Lyon
+('50 Boulevard des Oliviers', 13), -- Montpellier
+('9 Place de la Mairie', 14), -- Nancy
+('2 Rue des Cerisiers', 15), -- Nantes
+('18 Avenue du Soleil', 16), -- Nice
+('6 Rue de la Liberté', 17), -- Orléans
+('1 Avenue des Champs-Élysées', 18), -- Paris
+('25 Rue de la Défense', 18), -- Paris
+('10 Boulevard Pasteur', 19), -- Pau
+('5 Rue des Vignes', 20), -- Reims
+('22 Rue de la Seine', 21), -- Rouen
+('11 Rue des Étoiles', 22), -- Saint-Nazaire
+('17 Rue de la Cathédrale', 23), -- Strasbourg
+('3 Avenue du Midi', 24); -- Toulouse
---
+++ database/Filling Centre CESIs.sql
@@ -0,0 +1,27 @@
+INSERT INTO Centre (Nom_Centre, ID_City)
+VALUES
+('CESI Aix-en-Provence', 1), -- Aix-en-Provence
+('CESI Angoulême', 2), -- Angoulême
+('CESI Arras', 3), -- Arras
+('CESI Bordeaux', 4), -- Bordeaux
+('CESI Brest', 5), -- Brest
+('CESI Caen', 6), -- Caen
+('CESI Dijon', 7), -- Dijon
+('CESI Grenoble', 8), -- Grenoble
+('CESI La Rochelle', 9), -- La Rochelle
+('CESI Le Mans', 10), -- Le Mans
+('CESI Lille', 11), -- Lille
+('CESI Lyon', 12), -- Lyon
+('CESI Montpellier', 13), -- Montpellier
+('CESI Nancy', 14), -- Nancy
+('CESI Nantes', 15), -- Nantes
+('CESI Nice', 16), -- Nice
+('CESI Orléans', 17), -- Orléans
+('CESI Paris-La Défense', 18), -- Paris (La Défense)
+('CESI Paris-Nanterre', 18), -- Paris (Nanterre)
+('CESI Pau', 19), -- Pau
+('CESI Reims', 20), -- Reims
+('CESI Rouen', 21), -- Rouen
+('CESI Saint-Nazaire', 22), -- Saint-Nazaire
+('CESI Strasbourg', 23), -- Strasbourg
+('CESI Toulouse', 24); -- Toulouse
