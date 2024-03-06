1aa1f83dcb33dcb71482bc519b029eff7ed1d90b
haksolot <sean.lamet.mail@gmail.com>
2024-03-06 22:36:53 +0100

    adding options in choice when creating

---
+++ front/script/admin-dashboard-interactions.js
@@ -0,0 +1,27 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var createButton = document.getElementById('create-button');
+    var toBlur = document.getElementById('to-blur');
+    var choice = document.getElementById('create-choice');
+
+    createButton.addEventListener('click', function() {
+        toBlur.style.filter = "blur(5px)";
+        choice.style.display = 'flex'; // Affiche l'overlay
+    });
+
+});
+
+document.addEventListener('click', function(event) {
+    var createButton = document.getElementById('create-button');
+    var toBlur = document.getElementById('to-blur');
+    var choice = document.getElementById('create-choice');
+
+    if(event.target != createButton){
+        toBlur.style.filter = "blur(0px)";
+        choice.style.display = 'none';
+    }
+    else{
+
+    } 
+});
