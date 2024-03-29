b31d25c2dca5ac47ab2990696c5fcb515fb04c43
Enutro <noah.durtellede@gmail.com>
2024-03-29 11:00:51 +0100

    scripts fini

--- frontend/script/admin-dashboard-interactions.js
+++ frontend/script/admin-dashboard-interactions.js
@@ -3,30 +3,87 @@ function GoToCreatePilot(){
     window.location.href = 'account-create-pilote.html';
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    var createButton = document.getElementById('create-button');
-    var toBlur = document.getElementById('to-blur');
-    var choice = document.getElementById('create-choice');
+function GoToCreateStudent(){
+    window.location.href = 'account-create-student.html';
+}
 
-    createButton.addEventListener('click', function() {
+function GoToCreateOffer(){
+    window.location.href = 'offer-creation.html';
+}
+
+function GoToOffer(){
+    window.location.href = "offer-admin.html";
+}
+
+function ModifiePilot(){
+    window.location.href="account-edit-pilote.html";
+}
+
+function ModifieStudent(){
+    window.location.href="account-edit-student.html";
+}
+
+function ModifieEntreprise(){
+    window.location.href="company-edit.html";
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var settings = document.getElementById('create-button');
+    var toBlur = document.getElementById('to-blur');
+    var choices = document.getElementById('create-choice');
+
+    settings.addEventListener('click', function(event) {
+        event.stopPropagation(); // Empêche le clic de se propager au document
         toBlur.style.filter = "blur(5px)";
-        choice.style.display = 'flex'; // Affiche l'overlay
+        choices.style.display = 'flex'; // Affiche l'overlay
     });
 
+    document.addEventListener('click', function(event) {
+        // On vérifie si l'événement de clic est à l'extérieur de `choices`
+        if (!choices.contains(event.target) && choices.style.display === 'flex') {
+            toBlur.style.filter = "blur(0px)";
+            choices.style.display = 'none'; // Cache l'overlay
+        }
+    });
 });
 
-document.addEventListener('click', function(event) {
-    var createButton = document.getElementById('create-button');
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var settings = document.getElementById('settings');
     var toBlur = document.getElementById('to-blur');
-    var choice = document.getElementById('create-choice');
+    var choices = document.getElementById('create-choices');
 
-    if(event.target != createButton){
-        toBlur.style.filter = "blur(0px)";
-        choice.style.display = 'none';
-    }
-    else{
+    settings.addEventListener('click', function(event) {
+        event.stopPropagation(); // Empêche le clic de se propager au document
+        toBlur.style.filter = "blur(5px)";
+        choices.style.display = 'flex'; // Affiche l'overlay
+    });
 
-    } 
+    document.addEventListener('click', function(event) {
+        // On vérifie si l'événement de clic est à l'extérieur de `choices`
+        if (!choices.contains(event.target) && choices.style.display === 'flex') {
+            toBlur.style.filter = "blur(0px)";
+            choices.style.display = 'none'; // Cache l'overlay
+        }
+    });
 });
+
+
+// document.getElementById('search').addEventListener('keyup', function() {
+//     var input = document.getElementById('search').value.toUpperCase();
+//     var offers = document.getElementById('scroller').getElementsByClassName('offer-container');
+  
+//     for (var i = 0; i < offers.length; i++) {
+//       var offer = offers[i];
+//       if (offer.textContent.toUpperCase().indexOf(input) > -1) {
+//         offer.style.display = "";
+//       } else {
+//         offer.style.display = "none";
+//       }
+//     }
+//   });
+
+
 
 
---
+++ frontend/script/offer-creation.js
@@ -0,0 +1,16 @@
+function confirmer(){
+    var company_name_input = document.getElementById("company-name-input").value;
+    var post_input = document.getElementById("post-input").value;
+    var adress_input = document.getElementById("adress-input").value;
+    var description_input = document.getElementById("description-input").value;
+    var sector_input = document.getElementById("sector-input").value;
+    var money_input = document.getElementById("money-input").value;
+    var duration_input = document.getElementById("duration-input").value;
+    if(company_name_input === "" || post_input === "" || adress_input ==="" || description_input ==="" || sector_input ==="" || money_input ==="" || duration_input ===""){
+        alert("Toutes les cases ayant une * doivent être remplis")
+    }else {
+        console.log("Compte créer avec succès");
+        window.location.href = "dashboard-admin.html";
+        alert("Offre créer avec succès")
+    }
+} 
